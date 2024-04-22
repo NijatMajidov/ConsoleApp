1fd832651800ff8abad52040df4e395ef4a2efa4
ramzihasanov <bd54tkjwr@code.edu.az>
2024-04-22 17:25:33 +0400

    Start write store app

---
+++ StoreApp/StoreApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace StoreApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
